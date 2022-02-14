b6061bd336d8a3be5b923b30c30b32dc9ad78eae
Alice <alicemc@collum.ca>
2022-02-13 16:45:54 -0800

    Troubleshooting attempt

--- default.js
+++ default.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-console.log("default.js started successfully.");
-
 const { Pool } = require('pg');
 var pool = new Pool({
   connectionString: "postgres://rusmgvqfhtxhfj:ce7ce46007e9ef72f8df9734a8105ccebe1d6ac5939cf1c2b37a5200e1f63a88@ec2-3-227-195-74.compute-1.amazonaws.com:5432/d7vrcnlr3b50c1" 
@@ -99,5 +97,5 @@ app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/menu'))
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
